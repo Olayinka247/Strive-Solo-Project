05b59216db2d0b3614d3dc049e6a312a9c3496bf
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-01-31 15:17:49 +0000

    functions

--- soloproject.js
+++ soloproject.js
@@ -60,3 +60,69 @@ console.log("\n-------Answer Exercise F----------");
 console.log("\n -------- Answer G----------");
 me.skill.pop()
 console.log(me);
+
+console.log("\n -------------Answer Functions Exercise 1--------------");
+
+// JS Functions
+/* EXERCISE 1
+    Write a function called dice; it should randomize an integer number between 1 and 6.
+*/
+let dice = function (y) {
+    return Math.floor(Math.random() * y) +1
+}
+console.log(dice (6));
+
+
+console.log("\n -------------Answer Functions Exercise 2--------------");
+/* EXERCISE 2
+    Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+*/
+
+let whoIsBigger = function (g,o){
+    if (g > o){
+        return g
+    } else{
+        return o
+    } 
+}
+console.log(whoIsBigger (12, 4));
+
+console.log("\n -------------Answer Functions Exercise 3--------------");
+
+/* EXERCISE 3
+    Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+    Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+*/
+ let universe = ("I Love Programming")
+ const myArray = universe.split ("")
+ console.log(myArray);
+
+ console.log("\n -------------Answer Functions Exercise 4--------------");
+
+ /* EXERCISE 4
+    Write a function called deleteOne which receives a string and a boolean as parameters.
+    If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+*/
+const deleteOne = (str, boolean) => {
+    let withoutFirstLetter = str.slice(1)
+    let withoutLastLetter = str.slice(0, -1)
+    return boolean ? withoutFirstLetter : withoutLastLetter
+}
+console.log(deleteOne('strive', true))
+
+console.log("\n -------------Answer Functions Exercise 5--------------");
+/* EXERCISE 5
+   Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+   Ex.: onlyLetters("I have 4 dogs") => returns "I have  dogs"
+*/
+
+const onlyLetters = (dom) => {
+    let arr = dom.split('')
+    for(let i=0; i<arr.length; i++) {
+        if(!isNaN(arr[i])) {
+           arr[i] = ' '
+        }
+    }
+    return arr.join('')
+}
+console.log(onlyLetters('domain247coding'))
