09c5f38d747b0f0445f8940c2fd6c470b712de41
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-01-31 09:16:14 +0000

    solo project answer A and B

---
+++ soloproject.js
@@ -0,0 +1,21 @@
+// // JS Basics
+
+// /* EXERCISE A
+//   // Create a variable called test and assign a string value to it.
+
+console.log("\n -------------Answer Exercise A--------------");
+
+let test = "My Name"
+console.log(test);
+
+/* EXERCISE B
+    Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
+*/
+console.log("\n -------------Answer Exercise B------------------");
+
+ let a = 10
+ let b = 20
+ let sum = a + b
+ console.log(sum);
+
+ 
